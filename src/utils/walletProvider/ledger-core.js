090bbf910f7830d0399a6868eede1f2fdbb9de59
Arrowana <8245419+Arrowana@users.noreply.github.com>
2021-03-12 22:11:02 +1100

    hack code to get same derivation path

--- src/utils/walletProvider/ledger-core.js
+++ src/utils/walletProvider/ledger-core.js
@@ -61,7 +61,7 @@ function _harden(n) {
 }
 
 export function solana_derivation_path(account, change) {
-  const length = 4;
+  const length = 2;
   let useAccount = account ? account : 0;
   let useChange = change ? change : 0;
 
@@ -71,8 +71,8 @@ export function solana_derivation_path(account, change) {
   offset = derivation_path.writeUInt8(length, offset);
   offset = derivation_path.writeUInt32BE(_harden(44), offset); // Using BIP44
   offset = derivation_path.writeUInt32BE(_harden(501), offset); // Solana's BIP44 path
-  offset = derivation_path.writeUInt32BE(_harden(useAccount), offset);
-  derivation_path.writeUInt32BE(_harden(useChange), offset);
+  //offset = derivation_path.writeUInt32BE(_harden(useAccount), offset);
+  //derivation_path.writeUInt32BE(_harden(useChange), offset);
 
   return derivation_path;
 }
